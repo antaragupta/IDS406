7ec525a01a4cbf6bdd48f3b52a7ea46f63cb019e
Antara Gupta <Antaragupta@vikass-MacBook-Pro.local>
2018-04-15 23:24:46 -0500

    initial setup

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ModalController, NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public modalCtrl: ModalController, public navCtrl: NavController) {
 
   }
 
+  public openModal(){
+    let modalPage = this.modalCtrl.create('SignInModalPage');
+    modalPage.present();
+  }
 }
