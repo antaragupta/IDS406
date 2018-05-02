102fe9dfcd610ebe37c29625c3d374aa40e63222
Antara Gupta <Antaragupta@vikass-MacBook-Pro.local>
2018-05-02 08:48:00 -0500

    updates

--- src/pages/sign-in-modal/sign-in-modal.ts
+++ src/pages/sign-in-modal/sign-in-modal.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { FirebaseService } from './../../providers/firebase-service/firebase-service';
+import { AngularFireList } from 'angularfire2/database';
+import { AuthService } from './../../providers/auth-service/auth-service';
 /**
  * Generated class for the SignInModalPage page.
  *
@@ -15,11 +17,56 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignInModalPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public firebaseService: FirebaseService, public authService: AuthService) {
   }
-
+  userInputDetails = {
+    EmailID: '',
+    Password: ''
+  };
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignInModalPage');
   }
 
 }
+
+ signInFormSubmit(form) {
+  this.error = [];
+
+  if (this.userInputDetails.EmailID == null || this.userInputDetails.EmailID == "") {
+    this.error.push("Email ID invalid");
+  }
+
+  if (this.userInputDetails.Password == null || this.userInputDetails.Password == "") {
+    this.error.push("password is Invalid");
+  }
+
+  console.log(this.error.length);
+  if (this.error.length == 0) {
+    this.getUserlist();
+    let emailFlag = false;
+    for (let i = 1; i <= this.userList.length; i++) {
+      if (this.userList[i].EmailID != this.userInputDetails.EmailID) {
+        emailFlag = true;
+      }
+    }
+    if (!emailFlag) {
+      this.authService.signInWithEmailAndPassword(this.userInputDetails.EmailID, this.userInputDetails.Password)
+        .then(() => {
+          console.log('');
+        }).catch(_error => {
+          this.error = _error
+          console.log('');
+        });
+    }
+    console.log("Login Successful");
+  }
+  else {
+    this.error.push("Email ID doesn't match");
+    console.error(this.error);
+  }
+
+}
+
+
+
+
